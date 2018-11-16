78f85b89fd7e8d7010cba4a5a78d0979fbcb43c8
fabien lenoir <flenoir@gmail.com>
2018-11-16 18:04:35 +0100

    refactoring and add delete

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import requests
 from xml.etree import ElementTree as etree
@@ -51,15 +51,43 @@ CORS(app)
 def ping_pong():
     return jsonify('pong!')
 
-# Jobs route
-@app.route('/jobs', methods=['GET'])
+# # Jobs route
+# @app.route('/jobs', methods=['GET'])
+# def all_jobs():
+#     JOBS = request_data_from_jobs(request_current_jobs())
+#     return jsonify({
+#         'status': 'success',
+#         'jobs': JOBS
+#     })
+    
+
+# Jobs route GET and POST
+@app.route('/jobs', methods=['GET', 'POST'])
 def all_jobs():
     JOBS = request_data_from_jobs(request_current_jobs())
-    return jsonify({
-        'status': 'success',
-        'jobs': JOBS
-    })
+    response_object = {'status': 'success', 'jobs': JOBS}
+    if request.method == 'POST':
+        post_data = request.get_json()
+        print(post_data.get('title'))
+        # JOBS.append({
+        #     'title': post_data.get('title')
+        # })
+        response_object['message'] = 'Job added!'
+    else:
+        response_object['jobs'] = JOBS
+    return jsonify(response_object)
     
+#   # Jobs route DELETE
+@app.route('/jobs/<job_id>', methods=['DELETE','POST'])
+def del_job(job_id):
+    response_object = {'status' : 'success'}
+    print("received", job_id)
+    
+    if request.method == 'DELETE':
+        # remove_job(job_id)
+        remove_job = requests.delete('http://10.0.0.106:8647/CambriaFC/v1/Jobs/{}'.format(job_id))
+        response_object['message'] = "Job removed ! {}".format(remove_job)
+    return jsonify(response_object)  
 
 
 # -----------------------------------------------------    
@@ -102,21 +130,14 @@ def request_data_from_jobs(current_jobs):
             job_dict['Output'] = job.attrib['OutputFilename']
             job_dict['Priority'] = job.attrib['Priority']
             job_dict['End_Time'] = job.attrib['EndTime']
-            # DJOBS.append(job_dict)
+            job_dict['JobID'] = job.attrib['JobID']
 
         for x in tree_jobs.iter('Task'):
-            # print(x.tag, x.attrib['Progress'])
             job_dict['Progress'] = x.attrib['Progress']
-            DJOBS.append(job_dict)
-            # print('job_id', job.tag, job.attrib['JobID'])
-    
-    # print(DJOBS)  
+            DJOBS.append(job_dict) # on append DJOBS une fois qu'on atoutes les données
+            # print('job_id', job.tag)
+      
     return DJOBS
-
-# JOBS = request_data_from_jobs(request_current_jobs())
-# print(JOBS)
-
-
 
 
 if __name__ == '__main__':
--- client/src/components/Jobs.vue
+++ client/src/components/Jobs.vue
@@ -10,15 +10,14 @@
           <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
           FTC Jobs
         </a>
-        <button type="button" class="btn btn-success btn-sm">Add Job</button>
+        <button type="button" class="btn btn-success btn-sm" v-b-modal.job-modal>Add Job</button>
       </nav>
     </div>
 
     <div class="row">
       <div class="col-sm-12">
-        <!-- <h1 class="text-light">Jobs</h1> -->
-        <!-- <hr><br><br> -->
-        <!-- <button type="button" class="btn btn-success btn-sm">Add Job</button> -->
+        <!-- job board display -->
+
         <br><br>
         <table class="table table-hover table-dark table-striped table-hover table-sm">
           <thead>
@@ -30,6 +29,7 @@
               <th scope="col">Output</th>
               <th scope="col">Priority</th>
               <th scope="col">End Time</th>
+              <th scope="col">Job ID</th>
               <th></th>
             </tr>
           </thead>
@@ -50,14 +50,54 @@
               <td>{{ job.Output }}</td>
               <td>{{ job.Priority }}</td>
               <td>{{ job.End_Time}}</td>
+              <td>{{ job.JobID}}</td>
               <td>
-                <button type="button" class="btn btn-danger btn-sm">Delete</button>
+                <button type="button" class="btn btn-danger btn-sm" @click="onDeleteJob(job)">Delete</button>
               </td>
             </tr>
           </tbody>
         </table>
       </div>
     </div>
+
+
+    <!-- New job modal -->
+      <b-modal ref="addJobModal"
+            id="job-modal"
+            title="Add a new job"
+            hide-footer>
+      <b-form @submit="onSubmit" @reset="onReset" class="w-100">
+      <b-form-group id="form-title-group"
+                    label="Title:"
+                    label-for="form-title-input">
+          <b-form-input id="form-title-input"
+                        type="text"
+                        v-model="addJobForm.title"
+                        required
+                        placeholder="Enter title">
+          </b-form-input>
+        </b-form-group>
+        <b-form-group id="form-author-group"
+                      label="Author:"
+                      label-for="form-author-input">
+            <b-form-input id="form-author-input"
+                          type="text"
+                          v-model="addJobForm.author"
+                          required
+                          placeholder="Enter author">
+            </b-form-input>
+          </b-form-group>
+        <b-form-group id="form-read-group">
+          <b-form-checkbox-group v-model="addJobForm.read" id="form-checks">
+            <b-form-checkbox value="true">Read?</b-form-checkbox>
+          </b-form-checkbox-group>
+        </b-form-group>
+        <b-button type="submit" variant="primary">Submit</b-button>
+        <b-button type="reset" variant="danger">Reset</b-button>
+      </b-form>
+    </b-modal>
+
+
   </div>
 </template>
 
@@ -68,12 +108,19 @@ export default {
   data() {
     return {
       jobs: [],
+      addJobForm: {
+        title: '',
+        author: '',
+        read: [],
+      },
     };
   },
   methods: {
+    // get jobs
     getJobs() {
       const path = 'http://localhost:5001/jobs';
-       setInterval(() => {
+
+      setInterval(() => {
        axios.get(path)
         .then((res) => {
           this.jobs = res.data.jobs;
@@ -82,16 +129,63 @@ export default {
           // eslint-disable-next-line
           console.error(error);
         });
-    }, 30000);
-      // axios.get(path)
-      //   .then((res) => {
-      //     this.jobs = res.data.jobs;
-      //   })
-      //   .catch((error) => {
-      //     // eslint-disable-next-line
-      //     console.error(error);
-      //   });
+      }, 30000);
     },
+    // add jobs
+    addJob(payload) {
+        const path = 'http://localhost:5000/books';
+        axios.post(path, payload)
+          .then(() => {
+            this.getJobs();
+          })
+          .catch((error) => {
+            // eslint-disable-next-line
+            console.log(error);
+            this.getJobs();
+          });
+      },
+    initForm() {
+        this.addJobForm.title = '';
+        this.addJobForm.author = '';
+        this.addJobForm.read = [];
+    },
+    onSubmit(evt) {
+        evt.preventDefault();
+        this.$refs.addJobModal.hide();
+        let read = false;
+        if (this.addJobForm.read[0]) read = true;
+        const payload = {
+          title: this.addJobForm.title,
+          author: this.addJobForm.author,
+          read, // property shorthand
+        };
+        this.addJob(payload);
+        this.initForm();
+    },
+    onReset(evt) {
+        evt.preventDefault();
+        this.$refs.addJobModal.hide();
+        this.initForm();
+    },
+    // delete jobs
+    removeJob(JobID) {
+    const path = `http://localhost:5001/jobs/${JobID}`;
+    axios.delete(path)
+      .then(() => {
+        this.getJobs();
+        this.message = 'Job removed!';
+        this.showMessage = true;
+      })
+      .catch((error) => {
+        // eslint-disable-next-line
+        console.error(error);
+        this.getJobs();
+      });
+    },
+    onDeleteJob(job) {
+      this.removeJob(job.JobID);
+    },
+
   },
   created() {
     this.getJobs();
